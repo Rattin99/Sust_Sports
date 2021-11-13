6009aa5c6809ed34fb2a6eaa2c2b269b0e919d74
Kowshiq006 <asif.ahmed.kowshiq@gmail.com>
2021-11-13 23:04:08 +0600

    departments

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -25,6 +25,31 @@ const SignUp = () => {
                         <option value="Computer Science and Engineering"> Computer Science and Engineering</option>
                         <option value="Chemical Engineering & Polymer Science">Chemical Engineering & Polymer Science</option>
                         <option value="Architecture">Architecture</option>
+                        <option value="Forestry & Environmental Science">Forestry & Environmental Science</option>
+                        <option value="Mechanical Engineering">Mechanical Engineering</option>
+                        <option value="Petroleum and Mining Engineering">Petroleum and Mining Engineering</option>
+                        <option value="Industrial and Production Engineering">Industrial and Production Engineering</option>
+                        <option value="Food Engineering & Tea Technology">Food Engineering & Tea Technology</option>
+                        <option value="Electrical & Electronic Engineering">Electrical & Electronic Engineering</option>
+                        <option value="Civil & Environmental Engineering">Civil & Environmental Engineering</option>
+                        <option value="Biochemistry and Molecular Biology">Biochemistry and Molecular Biology</option>
+                        <option value="Genetic Engineering & Biotechnology">Genetic Engineering & Biotechnology</option>
+                        <option value="Business Administration">Business Administration</option>
+                        <option value="Chemistry">Chemistry</option>
+                        <option value="Geography and Environment">Geography and Environment</option>
+                        <option value="Mathematics">Mathematics</option>
+                        <option value="Physics">Physics</option>
+                        <option value="Statistics">Statistics</option>
+                        <option value="Bangla">Bangla</option>
+                        <option value="Economics">Economics</option>
+                        <option value="English">English</option>
+                        <option value="Political Studies">Political Studies</option>
+                        <option value="Public Administration">Public Administration</option>
+                        <option value="Social Work">Social Work</option>
+                        <option value="Sociology">Sociology</option>
+                        <option value="Oceanography">Oceanography</option>
+                        <option value="Software Engineering">Anthropology</option>
+                        <option value="Anthropology">Anthropology</option>
                     </select>
                 </div>
             </div>
